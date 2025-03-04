6bdaa9e834be0081f93f90e52ee71214995b3551
Lenflied <32714094+Lenflied@users.noreply.github.com>
2025-03-04 17:21:37 +0800

    Update_明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -1,7 +1,7 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
+let userID = '239d0206-9974-41a5-9344-fd2e688f261c';
 let proxyIP = '';
 //let sub = '';
 let subConverter = atob('U1VCQVBJLkNNTGl1c3Nzcy5uZXQ=');
